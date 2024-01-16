1435532f02a0252fee9f30128e3b44537174f07c
Manik <tonysins43@gmail.com>
2024-01-16 16:04:15 +0600

    refactor route and set route with dynamic way

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,9 +1,13 @@
+import { ReactNode } from "react";
 import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
-
-export const adminPaths2 = [
+type TRoute = {
+  path: string;
+  element: ReactNode;
+};
+export const adminPaths = [
   {
     name: "Dashboard",
     path: "dashboard",
@@ -31,21 +35,42 @@ export const adminPaths2 = [
   },
 ];
 
-export const adminPaths = [
-  {
-    path: "dashboard",
-    element: <AdminDashboard />,
-  },
-  {
-    path: "create-student",
-    element: <CreateStudent />,
-  },
-  {
-    path: "create-admin",
-    element: <CreateAdmin />,
-  },
-  {
-    path: "create-faculty",
-    element: <CreateFaculty />,
-  },
-];
+//* programmatical way --------------
+
+export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
+  if (item.path && item.element) {
+    acc.push({
+      path: item.path,
+      element: item.element,
+    });
+  }
+  if (item.children) {
+    item.children.forEach((child) => {
+      acc.push({
+        path: child.path,
+        element: child.element,
+      });
+    });
+  }
+  return acc;
+}, []);
+
+//! hard coded way ----------------
+// export const adminPaths = [
+//   {
+//     path: "dashboard",
+//     element: <AdminDashboard />,
+//   },
+//   {
+//     path: "create-student",
+//     element: <CreateStudent />,
+//   },
+//   {
+//     path: "create-admin",
+//     element: <CreateAdmin />,
+//   },
+//   {
+//     path: "create-faculty",
+//     element: <CreateFaculty />,
+//   },
+// ];
