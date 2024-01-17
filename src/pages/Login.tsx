87dd2752b5d150b62c1488499a742bef1f08d1e4
Manik <tonysins43@gmail.com>
2024-01-18 00:17:00 +0600

    setup baseApi with rtk query for login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,32 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
 const Login = () => {
-  return <div>Login page</div>;
+  const { register, handleSubmit } = useForm();
+  const [login, { data, error, isLoading }] = useLoginMutation();
+  console.log("data", data);
+  console.log("err", error);
+  const onSubmit = (data) => {
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    };
+    login(userInfo);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID:</label>
+        <input type="text" id="id" {...register("id")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
+  );
 };
 
 export default Login;
