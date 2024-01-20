d3b988becac7b9062e81a92ec18ce2d955d75f5a
Manik <tonysins43@gmail.com>
2024-01-20 16:54:20 +0600

    send refresh token to get access token

--- src/pages/admin/semesterManagement/AcademicSemester.tsx
+++ src/pages/admin/semesterManagement/AcademicSemester.tsx
@@ -3,7 +3,11 @@ import { useGetAllSemestersQuery } from "../../../redux/features/academicSemeste
 const AcademicSemester = () => {
   const { data } = useGetAllSemestersQuery(undefined);
   console.log(data);
-  return <div></div>;
+  return (
+    <div>
+      <h1>Academic semester</h1>
+    </div>
+  );
 };
 
 export default AcademicSemester;
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
@@ -13,9 +14,28 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+//* making costume base query
 const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
-  const result = await baseQuery(args, api, extraOptions);
-  console.log(result);
+  let result = await baseQuery(args, api, extraOptions);
+  // console.log(result);
+  if (result.error?.status === 401) {
+    //* send refresh token for get access token
+    const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
+      method: "POST",
+      credentials: "include",
+    });
+    const data = await res.json();
+    console.log(data);
+    const user = (api.getState() as RootState).auth.user;
+    api.dispatch(
+      setUser({
+        user,
+        token: data.data.accessToken,
+      })
+    );
+    result = await baseQuery(args, api, extraOptions);
+  }
+  return result;
 };
 export const baseApi = createApi({
   reducerPath: "baseApi",
