8887c1902ca4eadd5b1864531cbdf118876ab599
Manik <tonysins43@gmail.com>
2024-02-12 15:31:11 +0600

    modify student offered course

--- src/pages/student/OfferedCourse.tsx
+++ src/pages/student/OfferedCourse.tsx
@@ -3,7 +3,17 @@ import { useGetMyAllOfferedCoursesQuery } from "../../redux/features/student/stu
 const OfferedCourse = () => {
   const { data: studentOfferedCourse } =
     useGetMyAllOfferedCoursesQuery(undefined);
-  console.log(studentOfferedCourse);
+  const singleObject = studentOfferedCourse?.data?.reduce((acc, item) => {
+    const key = item?.course?.title;
+    acc[key] = acc[key] || { courseTitle: key, sections: [] };
+    acc[key].sections.push({
+      section: item?.section,
+      _id: item?._id,
+    });
+    return acc;
+  }, {});
+  // console.log(singleObject);
+  console.log(Object.values(singleObject ? singleObject : {}));
   return (
     <div>
       <h1>Student : offered course</h1>
