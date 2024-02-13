107f154f220b1b4c5206920408861e5e92f2424d
Manik <tonysins43@gmail.com>
2024-02-13 15:59:52 +0600

    successfully implete enroll course functionality

--- src/pages/student/OfferedCourse.tsx
+++ src/pages/student/OfferedCourse.tsx
@@ -1,37 +1,61 @@
 import { Button, Col, Row } from "antd";
-import { useGetMyAllOfferedCoursesQuery } from "../../redux/features/student/studentCourseManagement.api";
+import {
+  useEnrollCourseMutation,
+  useGetMyAllOfferedCoursesQuery,
+} from "../../redux/features/student/studentCourseManagement.api";
+import { TApiResponse } from "../../types";
+import { toast } from "sonner";
 
+type TCourse = {
+  [index: string]: any;
+};
 const OfferedCourse = () => {
   const { data: studentOfferedCourse } =
     useGetMyAllOfferedCoursesQuery(undefined);
-  const singleObject = studentOfferedCourse?.data?.reduce((acc, item) => {
-    const key = item?.course?.title;
-    acc[key] = acc[key] || { courseTitle: key, sections: [] };
-    acc[key].sections.push({
-      section: item?.section,
-      _id: item?._id,
-      days: item?.days,
-      startTime: item?.startTime,
-      endTime: item?.endTime,
-    });
-    return acc;
-  }, {});
+  const [enroll] = useEnrollCourseMutation();
+  const singleObject = studentOfferedCourse?.data?.reduce(
+    (acc: TCourse, item) => {
+      const key = item?.course?.title;
+      acc[key] = acc[key] || { courseTitle: key, sections: [] };
+      acc[key].sections.push({
+        section: item?.section,
+        _id: item?._id,
+        days: item?.days,
+        startTime: item?.startTime,
+        endTime: item?.endTime,
+      });
+      return acc;
+    },
+    {}
+  );
   // console.log(singleObject);
   const modifiedData = Object.values(singleObject ? singleObject : {});
   // console.log(modifiedData);
-  const handleEnroll = async (id) => {
+
+  const handleEnroll = async (id: string) => {
+    const toastId = toast.loading("Enrolling...");
     const enrollData = {
       offeredCourse: id,
     };
-
-    // const res = await enroll(enrollData);
-    // console.log(res);
+    try {
+      const res = (await enroll(enrollData)) as TApiResponse<any>;
+      if (res.error) {
+        toast.error(res.error.data.message, { id: toastId });
+      } else {
+        toast.success("Course enroll  successfully", { id: toastId });
+      }
+    } catch (error) {
+      toast.error("Something went wrong", { id: toastId });
+    }
   };
+  if (!modifiedData.length) {
+    return <p>No available courses</p>;
+  }
   return (
     <Row gutter={[0, 20]}>
-      {modifiedData.map((item) => {
+      {modifiedData.map((item, index: number) => {
         return (
-          <Col span={24} style={{ border: "solid #d4d4d4 2px" }}>
+          <Col key={index} span={24} style={{ border: "solid #d4d4d4 2px" }}>
             <div style={{ padding: "10px" }}>
               <h2>{item.courseTitle}</h2>
             </div>
